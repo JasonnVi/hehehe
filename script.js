08f4767aa42747c0b0048922d2ea06cecab1527a
Jasonn Villaver <156795078+JasonnVi@users.noreply.github.com>
2024-10-25 01:14:01 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function showInteraction() {
+    document.getElementById("interactiveSection").classList.remove("hidden");
+    document.getElementById("acceptButton").classList.add("hidden");
+}
+
+function sendVirtualHug() {
+    document.getElementById("responseSection").classList.remove("hidden");
+    alert("Virtual Hug Sent! 🤗💖");
+}
+
+function startMovie() {
+    alert("Let's start the movie and get cozy! 🍿🎬");
+}
